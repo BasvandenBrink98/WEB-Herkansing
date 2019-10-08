62beae0cc96ee27bdfdd5163786bd38b7a5534da
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2019-10-08 15:11:40 +0200

    font fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var btn = document.querySelector('#filterBtn');
 var form = document.querySelector('#filterForm');
-var filtersZichtbaar = false;
+var verhalen = document.querySelector('#verhalen');
+var fiterSection = document.querySelector('#filterSection');
+var filtersZichtbaar = true;
 console.log(filtersZichtbaar);
 
 function btnSwitch() {
@@ -8,10 +10,25 @@ function btnSwitch() {
    if (filtersZichtbaar == true) {
      form.classList.add('FilterOn');
      form.classList.remove('FilterOff');
+     verhalen.classList.add('StoriesLow');
+     verhalen.classList.remove('StoriesHigh');
+     btn.classList.add('btnActive');
+     btn.classList.remove('btnInactive');
+     console.log("test")
+     // filterSection.classList.add('formGrid');
+     // var h2 = document.createElement('h2');
+     // h2.textContent = 'Vind jouw perfecte match';
+     // filterSection.appendChild(h2);
      filtersZichtbaar = false;
    } else {
      form.classList.add('FilterOff');
      form.classList.remove('FilterOn');
+     verhalen.classList.add('StoriesHigh');
+     verhalen.classList.remove('StoriesLow');
+     btn.classList.add('btnInctive');
+     btn.classList.remove('btnActive');
+     // filterSection.classList.remove('formGrid');
+     // filterSection.removeChild(h2);
      filtersZichtbaar = true;
    }
 }
